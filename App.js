71d4ba140fa6b903d3c778c23651594daf9d82fe
Mum31 <168490361+Mum31@users.noreply.github.com>
2024-06-03 15:24:23 +0100

    react-native version of project

--- App.js
+++ App.js
@@ -1,38 +1,67 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { Login, Signup, Welcome } from "./screens";
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
 
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  
+const WelcomePage = ({ navigation }) => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='Welcome'
-      >
-        <Stack.Screen
-          name="Welcome"
-          component={Welcome}
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-          options={{
-            headerShown: false
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ImageBackground source={require('./assets/hero2.jpg')} style={styles.container}>
+      <View style={[styles.content, styles.contentBottom]}>
+        <Text style={styles.title}>Prêt pour un déménagement en toute tranquillité ?</Text>
+        <Text style={styles.subtitle}>Laissez-nous vous guider à travers le processus, de A à Z.</Text>
+        <TouchableOpacity
+          style={[styles.button, styles.primaryButton]}
+          onPress={() => navigation.navigate('Services')}
+        >
+          <Text style={[styles.buttonText, styles.primaryButtonText]}>C'est parti !</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+    padding: 30,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  contentBottom: {
+    marginBottom: 80,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#0077B6',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#333',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 6,
+  },
+  primaryButton: {
+    backgroundColor: '#0077B6',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  primaryButtonText: {
+    color: '#fff',
+  },
+});
+
+export default WelcomePage;
